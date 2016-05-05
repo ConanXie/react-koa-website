f7f2ea8791ec024b216eb421495096b0029f9560
ConanXie <xjj416@gmail.com>
2016-05-05 17:56:25 +0800

    add uglify js task

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,53 @@ const csso = require('gulp-csso')
 const notify = require('gulp-notify')
 const plumber = require('gulp-plumber')
 const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
 
 const files = [
-    'static/js/*.js',
-    'static/css/*.css',
-    'views/**',
-    'src/**'
+  'static/js/*.js',
+  'static/css/*.css',
+  'views/**',
+  'src/**'
 ]
 
 gulp.task('less', () => {
-    gulp.watch('static/**/*.less', (e) => {
-        if (e.type === 'changed') {
-            let path = e.path
-            gulp.src(path)
-                .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-                .pipe(less())
-                .pipe(csso())
-                .pipe(gulp.dest('static/css/'))
-        }
-    })
+  gulp.watch('static/**/*.less', (e) => {
+    if (e.type === 'changed') {
+      let path = e.path
+      gulp.src(path)
+        .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+        .pipe(less())
+        .pipe(csso())
+        .pipe(gulp.dest('static/css/'))
+    }
+  })
 })
 
 gulp.task('sync', () => {
-    browserSync.create().init({
-        proxy: 'localhost:4000',
-        notify: false,
-        files: files
-    })
+  browserSync.create().init({
+    proxy: 'localhost:4000',
+    notify: false,
+    files: files
+  })
 })
 
 gulp.task('default', ['less', 'sync'])
+
+/**
+ * 压缩js，转es2015
+ */
+
+require('babel-core/register')({
+  presets: ['es2015', 'react']
+})
+
+gulp.task('script', () => {
+  gulp.src('static/bundle.js')
+    .pipe(plumber())
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(uglify({ mangle: false }))
+    .pipe(concat('bundle.min.js'))
+    .pipe(gulp.dest('./static'))
+})
