ed35a8f3860856a5228107e912ea032c16af8925
conan <xjj416@outlook.com>
2016-05-22 22:44:55 +0800

    remove <Router> to main.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect } from 'react-router'
 
 import Root from './components/root'
 import Index from './components/index'
@@ -11,32 +11,19 @@ import About from './components/about'
 import NotFound from './components/404'
 import Counter from './components/counter'
 
-if (typeof window !== 'undefined') {
-  browserHistory.listen((location) => {
-    setTimeout(() => {
-      if (location.action === 'POP') {
-        return
-      }
-      window.scrollTo(0, 0)
-    })
-  })
-}
-
 const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Root}>
-      <IndexRoute component={Counter} />
-      <Route path="blog" component={Blog}>
-        <IndexRoute component={BlogList} />
-        <Route path="page/:page" component={BlogList} />
-        <Route path="detail/:id" component={BlogDetail} />
-      </Route>
-      <Route path="about" component={About} />
-      <Route path="data" component={About} />
+  <Route path="/" component={Root}>
+    <IndexRoute component={Counter} />
+    <Route path="blog" component={Blog}>
+      <IndexRoute component={BlogList} />
+      <Route path="page/:page" component={BlogList} />
+      <Route path="detail/:id" component={BlogDetail} />
     </Route>
+    <Route path="about" component={About} />
+    <Route path="data" component={About} />
     <Route path="404" component={NotFound} />
     <Redirect from="*" to="/404" />
-  </Router>
+  </Route>
 )
 
 export default routes
