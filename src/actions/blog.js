c53b2961bd8ef1e2c0efea1bfe1c1fb75c7c0726
conan <xjj416@outlook.com>
2016-05-22 22:45:33 +0800

    change file name and add getPagination action

---
+++ src/actions/blog.js
@@ -0,0 +1,28 @@
+import fetch from 'isomorphic-fetch'
+
+export const GET_BLOGLIST = 'GET_BLOGLIST'
+export const UPDATE_PAGINATION = 'UPDATE_PAGINATION'
+
+export function blogList(page = 1) {
+  return async (dispatch) => {
+    let response = await fetch(`/api/blog/list/${page}`)
+    let data = await response.json()
+    
+    dispatch({
+      type: GET_BLOGLIST,
+      data: data
+    })
+  }
+}
+
+export function getPagination(page = 1) {
+  return async (dispatch) => {
+    let response = await fetch(`/api/blog/pagination/${page}`)
+    let data = await response.json()
+    console.log(data)
+    dispatch({
+      type: UPDATE_PAGINATION,
+      pagination: data
+    })
+  }
+}
