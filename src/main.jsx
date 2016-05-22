3513fde3a1d4b5945d016bd5c62ce294d292fc80
conan <xjj416@outlook.com>
2016-05-22 22:42:43 +0800

    add react-router-redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { Router, browserHistory } from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 //Needed for onTouchTap
 injectTapEventPlugin()
@@ -7,6 +8,7 @@ injectTapEventPlugin()
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux'
 
 import reducer from './reducers/index'
 import routes from './routes'
@@ -14,18 +16,32 @@ import routes from './routes'
 let store
 if (window.__INITIAL_STATE__) {
   store = createStore(reducer, window.__INITIAL_STATE__, compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, routerMiddleware(browserHistory)),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ))
 } else {
   store = createStore(reducer, compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, routerMiddleware(browserHistory)),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ))
 }
 
+const history = syncHistoryWithStore(browserHistory, store)
+if (typeof window !== 'undefined') {
+  history.listen(location => {
+    setTimeout(() => {
+      if (location.action === 'POP') {
+        return
+      }
+      window.scrollTo(0, 0)
+    })
+  })
+}
+
+console.log(history)
+console.log(browserHistory)
 render((
   <Provider store={store}>
-    {routes}
+    <Router history={history} routes={routes} />
   </Provider>
 ), document.querySelector('#app'))
