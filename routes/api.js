5065313225342003f26a2799db136bb70d281494
conan <xjj416@outlook.com>
2016-05-22 22:40:03 +0800

    pagination api

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ const router = koaRouter()
  */
 const getBlogList = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile('./data/article.json', (err, data) => {
+    fs.readFile('./data/article.json', 'utf-8', (err, data) => {
       if (err) {
         reject(err)
       } else {
@@ -19,7 +19,7 @@ const getBlogList = () => {
   })
 }
 
-router.get('/bloglist/:page', async (ctx, next) => {
+router.get('/blog/list/:page', async (ctx, next) => {
   let data
   try {
     data = await getBlogList()
@@ -28,6 +28,58 @@ router.get('/bloglist/:page', async (ctx, next) => {
   }
   ctx.res.writeHead(200, { 'Content-Type': 'application/json' })
   ctx.body = data
+})
+
+/**
+ * 获取博客数目
+ */
+router.get('/blog/pagination/:page', async (ctx, next) => {
+  let size = 10
+  let page = + ctx.params.page
+  /**
+   * 获取文章总数
+   */
+  let data
+  try {
+    data = await getBlogList()
+  } catch (e) {
+    throw e
+  }
+  let length = JSON.parse(data).length
+  let total = 10 // Math.ceil(length / size)
+  /**
+   * 计算分页
+   */
+  let pagination = {
+    previous: 0,
+    current: 1,
+    next: 0
+  }
+  if (page > 0 && total >= page) {
+      pagination.previous = page - 1
+      pagination.current = page
+      pagination.next = page === total ? 0 : page + 1
+  } else {
+    pagination = {
+      code: '500',
+      msg: 'params error'
+    }
+  }
+  ctx.res.writeHead(200, { 'Content-Type': 'application/json' })
+  ctx.body = pagination
+})
+
+/**
+ * test
+ */
+router.post('/test/:param', ctx => {
+  console.log(ctx.request.body)
+  ctx.body = 'test'
+})
+router.get('/test/:param', ctx => {
+  console.log(ctx.query)
+  console.log(ctx.params)
+  ctx.body = 'haha'
 })
 
 export default router
