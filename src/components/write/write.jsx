4813b156789db038458d5aaa62c09a6671f38826
conan <xjj416@outlook.com>
2016-05-26 22:25:01 +0800

    write page

--- src/components/write/write.jsx
+++ src/components/write/write.jsx
@@ -4,6 +4,8 @@ import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
+import DatePicker from 'material-ui/DatePicker'
+import FlatButton from 'material-ui/FlatButton'
 import { lightBlue500 } from 'material-ui/styles/colors'
 
 const style = {
@@ -15,11 +17,28 @@ const style = {
   },
   floatingLabelStyle: {
     color: lightBlue500
+  },
+  textarea: {
+    width: '100%',
+    margin: '20px 0',
+    border: 'none',
+    outline: 'none',
+    fontFamily: 'inherit',
+    fontSize: '14px',
+    borderBottom: '1px solid #e0e0e0'
+  },
+  actionBox: {
+    textAlign: 'right'
   }
 }
 class Write extends Component {
   handleChange = (event, index, value) => {
     this.setState({value})
+  }
+  submitArticle = (e) => {
+    e.preventDefault()
+    console.log(this.refs.form)
+    return false
   }
   constructor(props) {
     super(props)
@@ -32,7 +51,7 @@ class Write extends Component {
         <div className="body">
           <Paper style={style.wrapper} zDepth={1}>
             <h1>write</h1>
-            <form name="write" ref="form">
+            <form name="write" ref="form" onSubmit={this.submitArticle}>
               <TextField
                 name="title"
                 ref="title"
@@ -47,13 +66,22 @@ class Write extends Component {
                 underlineFocusStyle={style.underlineStyle}
                 floatingLabelFocusStyle={style.floatingLabelStyle}
               /><br />
+              <DatePicker
+                hintText="发表时间"
+              />
               <SelectField value={this.state.value} onChange={this.handleChange}>
                 <MenuItem value={1} primaryText="Web" />
                 <MenuItem value={2} primaryText="Life" />
                 <MenuItem value={3} primaryText="IT" />
                 <MenuItem value={4} primaryText="Software" />
               </SelectField><br />
-              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <textarea style={style.textarea} name="body" id="" cols="30" rows="10" placeholder="文章内容"></textarea>
+              <div className="action-box" style={style.actionBox}>
+                <FlatButton label="取消" />
+                <FlatButton label="确认" secondary={true} onClick={this.submitArticle}>
+                  <input type="submit" hidden />
+                </FlatButton>
+              </div>
             </form>
           </Paper>
         </div>
