f16a969b7141027317b1d2f8c7f1c4b16e43261f
ConanXie <xjj416@gmail.com>
2016-06-16 18:46:47 +0800

    add a snackbar and check input

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,7 +1,31 @@
 export const LOG_IN = 'LOG_IN'
+export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR'
 
 export function login(userName, password) {
   return async (dispath) => {
+    let status = false
+    let snackbar = {
+      open: true,
+      message: ''
+    }
+    if (!userName) {
+      snackbar.message = '请输入用户名'
+      dispath({
+        type: LOG_IN,
+        status,
+        snackbar
+      })
+      return
+    }
+    if (!password) {
+      snackbar.message = '请输入密码'
+      dispath({
+        type: LOG_IN,
+        status,
+        snackbar
+      })
+      return
+    }
     let response = await fetch('/api/login', {
       method: 'post',
       headers: {
@@ -10,10 +34,25 @@ export function login(userName, password) {
       body: `user=${userName}&pw=${password}`
     })
     let data = await response.json()
+    snackbar.message = data.msg
+    if (!data.code) {
+      status = true
+    }
 
     dispath({
       type: LOG_IN,
-      payload: data
+      status,
+      snackbar
     })
+  }
+}
+
+export function closeSnackbar() {
+  return {
+    type: CLOSE_SNACKBAR,
+    snackbar: {
+      open: false,
+      message: ''
+    }
   }
 }
