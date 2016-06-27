cf0214248be35188d6d363f8732c40a2076409f1
ConanXie <xjj416@gmail.com>
2016-06-27 19:59:28 +0800

    the action get profits

---
+++ src/actions/profits.js
@@ -0,0 +1,13 @@
+export const GET_PROFITS ='GET_PROFITS'
+
+export function getProfits(page = 1) {
+  return async (dispatch) => {
+    let response = await fetch(`/api/profits/${page}`)
+    let data = await response.json()
+
+    dispatch({
+      type: GET_PROFITS,
+      data: data
+    })
+  }
+}
