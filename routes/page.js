eaf0e33a1394d7e14e54a783c60c5b31705f01c1
ConanXie <xjj416@gmail.com>
2016-08-29 21:35:48 +0800

    rename

--- routes/page.js
+++ routes/page.js
@@ -21,17 +21,65 @@ import routes from '../src/routes'
  * 生成服务器端store和匹配routes
  */
 const storeAndRoutes = (ctx, state) => {
-  let store = createStore(reducer, state, applyMiddleware(thunk))
+  const store = createStore(reducer, state, applyMiddleware(thunk))
   // 匹配react-router
+  /*return new Promise((resolve, reject) => {
+    match({ routes, location: ctx.url }, (error, redirectLocation, renderProps) => {
+      const render = ctx.render('index', {
+        root: renderToString(<h1>123</h1>),
+        initialState: JSON.stringify(store.getState())
+      })
+      if (!error) {
+        resolve(render)
+      }
+    })
+  })*/
   match({ routes, location: ctx.url }, (error, redirectLocation, renderProps) => {
-    ctx.render('index', {
+    /*ctx.render('index', {
       root: renderToString(
         <Provider store={store}>
           <RouterContext {...renderProps} />
         </Provider>
       ),
       initialState: JSON.stringify(store.getState())
-    })
+    })*/
+    const root = renderToString(
+      <Provider store={store}>
+        <RouterContext {...renderProps} />
+      </Provider>
+    )
+    const initialState = JSON.stringify(store.getState())
+    ctx.body = `
+      <!DOCTYPE html>
+      <html lang="zh">
+      <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+          <title>conan</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon-180x180.png" />
+          <link rel="icon" type="image/png" href="/images/favicons/android-chrome-192x192.png" sizes="192x192" />
+          <link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32" />
+          <link rel="shortcut icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
+          <meta name="theme-color" content="#0288D1">
+          <link rel="stylesheet" href="/css/default.css" />
+      </head>
+      <body>
+          <div id="app">
+              <div>${root}</div>
+          </div>
+          <footer>
+              <p>&copy;2016 xie jie. All rights reserved.</p>
+          </footer>
+          <script src="/js/lib/jquery.min.js"></script>
+          <script src="/js/main.js"></script>
+          <script>
+            window.__INITIAL_STATE__ = ${initialState}
+          </script>
+          <script src="/assets/bundle.js"></script>
+      </body>
+      </html>
+    `
   })
 }
 /**
@@ -40,6 +88,7 @@ const storeAndRoutes = (ctx, state) => {
 const apiUrl = 'http://localhost:4000/api'
 
 router.get('/', async (ctx, next) => {
+  console.log(ctx.state)
   let articles = await getArticles()
   let counter = {
     count: 100,
