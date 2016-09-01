d0cf20f9e1b7403ad587d7112180c3e730cf0a7c
ConanXie <xjj416@gmail.com>
2016-09-01 23:39:55 +0800

    add session

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-'use strict'
-
 import Koa from 'koa'
 import koaRouter from 'koa-router'
-import nunjucks from 'koa-nunjucks-2'
-import statics from 'koa-static'
+import views from 'koa-views'
+import serve from 'koa-static'
 import convert from 'koa-convert'
-import co from 'co'
 import bodyparser from 'koa-bodyparser'
-// import session from 'koa-session'
 import session from 'koa-generic-session'
 import redis from 'koa-redis'
-// import flash from 'koa-flash'
 
 const app = new Koa()
 const router = koaRouter()
@@ -19,33 +14,24 @@ import webpack from 'webpack'
 const webpackConfig = require('./config/webpack.config')
 const compiler = webpack(webpackConfig)
 
-/*app.use(async (ctx, next) => {
-  console.log(ctx.session)
-  let n = ctx.session.views || 0
-  ctx.session.views = ++n
-  console.log(ctx.session.views)
-  await next()
-})*/
-
 /**
  * 中间件
  */
-app.keys = ['user']
+app.keys = ['my-website']
 app.use(convert(session({
   store: redis()
 })))
-/*app.use(flash({
-  key: 'flash'
-}))*/
+
 app.use(bodyparser())
 
-/*app.use(async (ctx, next) => {
-  // ctx.session.count = ctx.session.count ? ++ctx.session.count : 1
-  console.log(ctx.session)
-  await next()
-})*/
-
-app.use(router.routes())
+app
+   .use(views(__dirname + '/views', {
+     map: {
+       html: 'nunjucks'
+     },
+     extension: 'html'
+   }))
+   .use(router.routes())
    .use(router.allowedMethods())
    .use(convert(require('koa-webpack-dev-middleware')(compiler, {
       publicPath: webpackConfig.output.publicPath,
@@ -57,16 +43,13 @@ app.use(router.routes())
       }
     })))
    .use(convert(require('koa-webpack-hot-middleware')(compiler)))
-   .use(convert(statics('static')))
+   .use(serve('static'))
 
-
-app.context.render = co.wrap(nunjucks({
-  ext: 'html',
-  path: 'views',
-  nunjucksConfig: {
-    autoescape: false
-  }
-}))
+/**
+ * 路由
+ */
+import routes from './routes'
+routes(app)
 
 /**
  * [404页面]
@@ -83,25 +66,7 @@ app.use(async (ctx, next) => {
   ctx.redirect('/404')
 })
 
-/**
- * 路由
- */
-import index from './routes/index'
-import api from './routes/api'
-// import test from './routes/test'
-router.use('/', index.routes())
-router.use('/api', api.routes())
-// router.use('/test', test.routes())
 
 app.listen(4000, () => {
   console.log('Listening on 4000')
 })
-
-/*app.context.render = co.wrap(render(app, {
-    root: 'views',
-    locals: 'static',
-    layout: false,
-    cache: false,
-    debug: true
-
-}))*/
