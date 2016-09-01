fa847e5369774279abae34a8396954065cb430bb
ConanXie <xjj416@gmail.com>
2016-09-01 23:44:44 +0800

    transition

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component, cloneElement } from 'react'
 import { Link } from 'react-router'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
 import RaisedButton from 'material-ui/RaisedButton'
 import AppBar from 'material-ui/AppBar'
 import Paper from 'material-ui/Paper'
@@ -14,12 +16,24 @@ const muiTheme = getMuiTheme(null, {userAgent: 'all'})
 
 class Root extends Component {
   render() {
+    const { children, location } = this.props 
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <Head title="conan" />
           <BackToTop />
-          {this.props.children}
+          {/*this.props.children*/}
+          <ReactCSSTransitionGroup
+            component="div"
+            className="page-wrapper"
+            transitionName="example"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}
+          >
+            {cloneElement(children, {
+              key: location.pathname
+            })}
+          </ReactCSSTransitionGroup>
         </div>
       </MuiThemeProvider>
     )
